0e3b4a378545c28181cfb67e8681d83152660fab
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2022-01-19 01:08:54 +0200

    add fonts for AppHeader

---
+++ frontend/src/pages/BoardApp.jsx
@@ -0,0 +1,49 @@
+import React, { Component, useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+
+// only for now //
+import { storageService } from '../services/async-storage.service'
+
+
+// TODO: Connect To STORE - Action
+
+function _BoardApp() {
+    // const [isMenuOpen, setMenuOpen] = useState(false)
+
+    useEffect(() => {
+        _loadBoard()
+        return () => {
+
+        }
+    }, [])
+
+    // useEffect(() => {
+    // }, [])
+
+    // const addMsg = (newMsg) => {
+    //     setMsgs(prevMsgs => [...prevMsgs, newMsg])
+    //     if (!toy.msgs) toy.msgs = [];
+    //     toy.mags.push(newMsg)
+    //     // action
+    // }
+
+    const _loadBoard = async () => {
+        const data = storageService.query();
+        if (data) console.log(data);
+    };
+
+    return (
+        <div className="bopard-app">
+            <h1>board app</h1>
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+    }
+}
+const mapDispatchToProps = {
+}
+
+export const BoardApp = connect(mapStateToProps, mapDispatchToProps)(_BoardApp)
