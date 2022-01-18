e8a521f0efa3af2abb7e832d959d32b5c3c5e8dd
Ori Ben Amram <benamram.ori@gmail.com>
2022-01-18 22:35:03 +0200

    fixing import problems

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 // import heroImg from '../assets/img/hero.png';
-=======
-import heroImg from '../assets/imgs/hero.png';
->>>>>>> b126045ca8f44452a63a17f66096f315b07f3fc5
 
 export function HomePage() {
   return (
