26d4e164f28b8947d251536b2c9de922d5042b5c
Ori Ben Amram <benamram.ori@gmail.com>
2022-01-19 21:24:13 +0200

    first-half-details-page pixel perfect basic

--- frontend/src/pages/TaskDetails.jsx
+++ frontend/src/pages/TaskDetails.jsx
@@ -1,4 +1,5 @@
 
+import { AiOutlineClose } from "react-icons/ai";
 import { AiOutlineCreditCard } from "react-icons/ai";
 export function TaskDetails() {
 
@@ -14,7 +15,7 @@ export function TaskDetails() {
                 </section>
                 {/* Details-header */}
                 <section className='details-header'>
-                    <AiOutlineCreditCard className='header-icon'/>
+                    <AiOutlineCreditCard className='header-icon' />
                     <textarea>app header bla bla</textarea>
                     <div className="header-sub-title">
                         <span>by Ori Ben Amram</span>
@@ -25,24 +26,35 @@ export function TaskDetails() {
                     {/* Main-Col */}
                     <section className='main-col'>
                         {/* Description */}
-                        <div className='description'>
-                            <span>app description ma ma</span>
+                        <div className='description-container'>
+                            <div className="description-title-container">
+                                <h3>Description</h3>
+                            </div>
+                            <textarea placeholder="Add a more detailed description..." className="description-text-area"></textarea>
+                            <div className="description-edit-container">
+                                <div>
+                                    <button className="save-btn">Save</button>
+                                    <button className="primary-close-btn">X</button>
+                                </div>
+                                <button>Formatting help</button>
+                            </div>
                         </div>
                         {/* Activities */}
                         <div className='activities'>
-
+                            Activities
                         </div>
                     </section>
 
                     {/* Side-Bar */}
                     <section className='side-bar'>
                         <section className='suggested'>
-
+                            suggested
                         </section>
                         <section className='add-to-card'>
-
+                            add to card
                         </section>
                         <section className='actions'>
+                            actions
                         </section>
                     </section>
 
