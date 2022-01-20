19e448cf6183c1eaa1174ad2d8079350722ed39c
Yuval <yuvalshai95@gmail.com>
2022-01-20 16:27:07 +0200

    D&D start

--- frontend/src/cmps/board/GroupList.jsx
+++ frontend/src/cmps/board/GroupList.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import {Droppable} from 'react-beautiful-dnd';
 import {DynamicAddAction} from './DynamicAddAction.jsx';
 import {GroupPreview} from './GroupPreview.jsx';
 
 export function GroupList({board}) {
   return (
-    <section className="group-list-container flex">
-      {board.groups.map(group => (
-        <GroupPreview key={group.id} boardId={board._id} group={group} />
-      ))}
-      <DynamicAddAction isList boardId={board._id} />
-    </section>
+    <Droppable droppableId={'all-groups'}>
+      {provided => (
+        <section className="group-list-container flex" {...provided.droppableProps} ref={provided.innerRef}>
+          {board.groups.map(group => (
+            <GroupPreview key={group.id} boardId={board._id} group={group} />
+          ))}
+          <DynamicAddAction isList boardId={board._id} />
+        </section>
+      )}
+    </Droppable>
   );
 }
--- frontend/src/pages/BoardApp.jsx
+++ frontend/src/pages/BoardApp.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
+import {DragDropContext} from 'react-beautiful-dnd';
 
 // CMPS
 import {GroupList} from '../cmps/board/GroupList.jsx';
@@ -12,6 +13,7 @@ export function BoardApp(props) {
   const dispatch = useDispatch();
   const board = useSelector(state => state.boardModule.board);
   const {id} = props.match.params;
+  const style = board ? board.style : '';
 
   useEffect(async () => {
     try {
@@ -22,12 +24,19 @@ export function BoardApp(props) {
     }
   }, []);
 
+  const onDragEnd = () => {
+    //TODO: reordering logic
+    console.log('Drag End');
+  };
+
   if (!board) return <Loader />;
   return (
-    <div className="board-app">
-      <h1>board app</h1>
-      {/* <BoardHeader /> */}
-      <GroupList board={board} />
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="board-app" style={{background: style.background}}>
+        <h1>board app</h1>
+        {/* <BoardHeader /> */}
+        <GroupList groups={[...board.groups]} boardId={board._id} board={board} />
+      </div>
+    </DragDropContext>
   );
 }
