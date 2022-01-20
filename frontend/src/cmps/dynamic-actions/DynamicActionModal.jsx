c9333fdca1dc5a13a2659633512791c164dff877
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2022-01-20 21:03:36 +0200

    frontend dynamic

--- frontend/src/cmps/dynamic-actions/DynamicActionModal.jsx
+++ frontend/src/cmps/dynamic-actions/DynamicActionModal.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 // CMPS
 import { MembersModalContent } from './MembersModalContent.jsx'
-
+import { CheckListModalContent } from './CheckListModalContent.jsx'
 export function DynamicActionModal({ toggleModal, type, pos }) {
     console.log('pos.clientY:', pos.clientY);
 
@@ -14,7 +14,7 @@ export function DynamicActionModal({ toggleModal, type, pos }) {
             case 'members':
                 return <MembersModalContent task />
             case 'checklist':
-                // return <MembersModalContent task />
+                return <CheckListModalContent />
         }
     }
     console.log('pos:', pos);
@@ -30,7 +30,7 @@ export function DynamicActionModal({ toggleModal, type, pos }) {
             <section className='modal-content'>
                 <input placeholder={`Search ${type}...`} type="text" className='modal-main-input' autoFocus />
                 {/* CONTENT */}
-                {/* {getContentForDisplay()} */}
+                {getContentForDisplay()}
             </section>
         </section>
     );
