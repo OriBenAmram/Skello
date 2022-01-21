a822272d4e14deeb9018824639ce7340084b6622
Ori Ben Amram <benamram.ori@gmail.com>
2022-01-21 14:33:30 +0200

    labels-modal full design

--- frontend/src/cmps/dynamic-actions/DynamicActionModal.jsx
+++ frontend/src/cmps/dynamic-actions/DynamicActionModal.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 
+import { GrClose } from "react-icons/gr";
+
 // CMPS
 import { MembersModalContent } from './MembersModalContent.jsx'
 import { LabelsModalContent } from './LabelsModalContent.jsx'
 import { CheckListModalContent } from './CheckListModalContent.jsx'
+
 export function DynamicActionModal({ toggleModal, type, pos, task, board }) {
 
     const [modalContent, setModalContent] = useState({ isSearch: true });
+    const [selectedLabel, setSelectedLabel] = useState(null);
 
     // Switch if that decide if
     const getContentForDisplay = () => {
@@ -21,23 +25,27 @@ export function DynamicActionModal({ toggleModal, type, pos, task, board }) {
     }
 
     const getPositionByType = () => {
-        // switch (type) {
-        //     case 'members':
-        //         return {  }
-        //     case 'labels':
-        //         return <LabelsModalContent task={task} board={board} />
-        //     case 'checklist':
-        //         return <CheckListModalContent />
-        // }
+        const { clientX, clientY } = pos
+        console.log('clientY:', clientY);
+        console.log('clientX:', clientX);
+        switch (type) {
+            case 'labels':
+                return { clientY: clientY - 300, clientX: 770 }
+            default:
+                return { clientY, clientX: 732 }
+        }
     }
+
+    const modalPosition = getPositionByType()
+    console.log('modalPosition:', modalPosition);
 
 
     return (
         // <section className='dynamic-action-modal' style={{ top: `${pos.clientY}px`, left: `${pos.clientX}px` }} >
-        <section className='dynamic-action-modal' style={{ top: `${pos.clientY}px`, left: `732px` }} >
+        <section className='dynamic-action-modal' style={{ top: `${modalPosition.clientY}px`, left: `${modalPosition.clientX}px` }} >
             <section className='modal-header'>
-                <button className='simple-close-btn' onClick={toggleModal}>x</button>
-                {type}
+                <button className='simple-close-btn' onClick={toggleModal}><GrClose className='btn-content'/></button>
+                {type.charAt(0).toUpperCase() + type.slice(1)}
             </section>
             <section className='modal-content'>
                 <input placeholder={`Search ${type}...`} type="text" className='modal-main-input' autoFocus />
