b6e5da0cb07630ad73d2f0493e71673b5e77e004
Ori Ben Amram <benamram.ori@gmail.com>
2022-01-23 16:06:10 +0200

    general colors, new label modal when clicking plus label in task details

--- frontend/src/cmps/dynamic-actions/DynamicActionModal.jsx
+++ frontend/src/cmps/dynamic-actions/DynamicActionModal.jsx
@@ -8,19 +8,14 @@ import { LabelsModalContent } from './LabelsModalContent.jsx'
 import { CheckListModalContent } from './CheckListModalContent.jsx'
 import { AttachmentModalContent } from "./AttachmentModalContent.jsx";
 
-export function DynamicActionModal({ toggleModal, type, pos, task, group, board, onSetModalType }) {
-
-    const [modalContent, setModalContent] = useState(null);
-    const [modalHeader, setModalHeader] = useState(null);
-
-
-    // Switch if that decide if
+export function DynamicActionModal({ toggleModal, type, pos, task, group, board}) {
+    
     const getContentForDisplay = () => {
         switch (type) {
             case 'members':
                 return <MembersModalContent />
             case 'labels':
-                return <LabelsModalContent toggleModal={toggleModal} onSetModalType={onSetModalType} task={task} group={group} board={board} />
+                return <LabelsModalContent toggleModal={toggleModal} task={task} group={group} board={board} />
             case 'checklist':
                 return <CheckListModalContent toggleModal={toggleModal} task={task} group={group} board={board} />
             case 'attachment':
@@ -43,16 +38,8 @@ export function DynamicActionModal({ toggleModal, type, pos, task, group, board,
 
 
     return (
-        // <section className='dynamic-action-modal' style={{ top: `${pos.clientY}px`, left: `${pos.clientX}px` }} >
         <section className='dynamic-action-modal' style={{ top: `${modalPosition.clientY}px`, left: `${modalPosition.clientX}px` }} >
             {getContentForDisplay()}
-            {/* <section className='modal-header'>
-                <button className='simple-close-btn' onClick={toggleModal}><GrClose className='btn-content'/></button>
-                {type.charAt(0).toUpperCase() + type.slice(1)}
-            </section>
-            <section className='modal-content'>
-                {getContentForDisplay()}
-            </section> */}
         </section>
-    );
+    )
 }
