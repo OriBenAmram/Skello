c65571b7ff5fdd4d63c61fc5584ddc374d2769e7
Yuval <yuvalshai95@gmail.com>
2022-01-25 22:00:17 +0200

    *http service *socket service

---
+++ frontend/src/services/socket.service.js
@@ -0,0 +1,70 @@
+import io from 'socket.io-client';
+
+export const SOCKET_EMIT_JOIN_BOARD = 'join-board';
+export const SOCKET_EMIT_JOIN_BOARD = 'new-board';
+export const SOCKET_EVENT_UPDATED_BOARD = 'updated-board';
+export const SOCKET_EVENT_BOARD_ADDED = 'new-board-added';
+
+const baseUrl = process.env.NODE_ENV === 'production' ? '' : '//localhost:3030';
+export const socketService = createSocketService();
+// export const socketService = createDummySocketService()
+// window.socketService = socketService
+
+socketService.setup();
+
+function createSocketService() {
+  var socket = null;
+  const socketService = {
+    setup() {
+      socket = io(baseUrl);
+    },
+    on(eventName, cb) {
+      socket.on(eventName, cb);
+    },
+    off(eventName, cb = null) {
+      if (!socket) return;
+      if (!cb) socket.removeAllListeners(eventName);
+      else socket.off(eventName, cb);
+    },
+    emit(eventName, data) {
+      socket.emit(eventName, data);
+    },
+    terminate() {
+      socket = null;
+    },
+  };
+  return socketService;
+}
+
+// eslint-disable-next-line
+function createDummySocketService() {
+  var listenersMap = {};
+  const socketService = {
+    listenersMap,
+    setup() {
+      listenersMap = {};
+    },
+    terminate() {
+      this.setup();
+    },
+    on(eventName, cb) {
+      listenersMap[eventName] = [...(listenersMap[eventName] || []), cb];
+    },
+    off(eventName, cb) {
+      if (!listenersMap[eventName]) return;
+      if (!cb) delete listenersMap[eventName];
+      else listenersMap[eventName] = listenersMap[eventName].filter(l => l !== cb);
+    },
+    emit(eventName, data) {
+      if (!listenersMap[eventName]) return;
+      listenersMap[eventName].forEach(listener => {
+        listener(data);
+      });
+    },
+    debugMsg() {
+      this.emit('chat addMsg', {from: 'Someone', txt: 'Aha it worked!'});
+    },
+  };
+  window.listenersMap = listenersMap;
+  return socketService;
+}
