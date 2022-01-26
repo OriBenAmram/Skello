963f0278bfc5589fbdb40c74e4d9b06cb1795be4
Yuval <yuvalshai95@gmail.com>
2022-01-26 23:00:20 +0200

    *sockets

--- frontend/src/pages/BoardApp.jsx
+++ frontend/src/pages/BoardApp.jsx
@@ -1,26 +1,43 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { DragDropContext } from 'react-beautiful-dnd';
-import { Route } from 'react-router-dom';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {DragDropContext} from 'react-beautiful-dnd';
+import {Route} from 'react-router-dom';
+import {socketService} from '../services/socket.service.js';
 
 // Cmps
-import { GroupList } from '../cmps/board/GroupList.jsx';
-import { Loader } from '../cmps/Loader.jsx';
-import { TaskDetails } from './TaskDetails.jsx';
-import { BoardHeader } from '../cmps/board/BoardHeader.jsx';
+import {GroupList} from '../cmps/board/GroupList.jsx';
+import {Loader} from '../cmps/Loader.jsx';
+import {TaskDetails} from './TaskDetails.jsx';
+import {BoardHeader} from '../cmps/board/BoardHeader.jsx';
 
 // Action
-import { loadBoard, handleDrag } from '../store/board/board.action';
+import {loadBoard, handleDrag, setBoard} from '../store/board/board.action';
 
 export function BoardApp(props) {
   const dispatch = useDispatch();
   const board = useSelector(state => state.boardModule.board);
-  const { id } = props.match.params;
+  const {id} = props.match.params;
 
   useEffect(async () => {
+    socketService.setup();
+    // add to all sockets board id
+    socketService.emit('join-board', id);
+    // get updated board from backend
+    socketService.off('updated-board');
+    socketService.on('updated-board', async updatedBoard => {
+      console.log('UPDATED board from backend', updatedBoard);
+      await dispatch(setBoard(updatedBoard));
+    });
     try {
       await dispatch(loadBoard(id));
-    } catch (err) { }
+    } catch (err) {}
+
+    return () => {
+      socketService.off('updated-board', () => {
+        console.log('I RUN FROM SOCKET OFF IN UNMOUNT');
+      });
+      socketService.terminate();
+    };
   }, []);
 
   const onDragEnd = result => {
@@ -28,7 +45,7 @@ export function BoardApp(props) {
     // Source - start index
     // Destination - end index(where was it dropped)
     // Type - group or task
-    const { destination, source, type } = result;
+    const {destination, source, type} = result;
 
     if (!destination) return;
 
@@ -38,14 +55,14 @@ export function BoardApp(props) {
   };
 
   console.log('board - BoardApp', board);
-  
 
   if (!board) return <Loader />;
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <div className='board-app-wrapper'  style={{ background: `${board.style.background}  center center / cover` }}>
+      <div
+        className="board-app-wrapper"
+        style={{background: `${board.style.background}  center center / cover`}}>
         <div className="board-app">
-
           <BoardHeader board={board} />
           <GroupList groups={[...board.groups]} boardId={board._id} board={board} />
         </div>
