9ec4ef781e572ae8d60c1e4d129f5c5477a2fa38
Yuval <yuvalshai95@gmail.com>
2022-01-27 00:43:07 +0200

    board header map unique key

--- frontend/src/cmps/board/BoardHeader.jsx
+++ frontend/src/cmps/board/BoardHeader.jsx
@@ -1,39 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { IoPersonAddOutline, IoStarOutline, IoEllipsisHorizontalSharp, IoBarChart } from 'react-icons/io5';
-import { useDispatch } from 'react-redux';
+import React, {useState, useEffect} from 'react';
+import {IoPersonAddOutline, IoStarOutline, IoEllipsisHorizontalSharp, IoBarChart} from 'react-icons/io5';
+import {useDispatch} from 'react-redux';
 
 // Cmps
-import { DynamicActionModal } from '../dynamic-actions/DynamicActionModal.jsx';
+import {DynamicActionModal} from '../dynamic-actions/DynamicActionModal.jsx';
 
 // action
 
-import { toggleSideMenu } from '../../store/app/app.action';
+import {toggleSideMenu} from '../../store/app/app.action';
 
-export function BoardHeader({ board }) {
-  const { title, members } = board;
+export function BoardHeader({board}) {
+  const {title, members} = board;
   const dispatch = useDispatch();
-  const [modal, setModal] = useState({ isModalOpen: false, type: null });
+  const [modal, setModal] = useState({isModalOpen: false, type: null});
 
   const toggleModal = props => {
-    const { event, type } = props;
+    const {event, type} = props;
     if (modal.isModalOpen) {
-      setModal({ ...modal, isModalOpen: false });
+      setModal({...modal, isModalOpen: false});
       return;
     }
-    setModal({ isModalOpen: true, type, event });
+    setModal({isModalOpen: true, type, event});
   };
-
 
   const onToggleMenu = () => {
     dispatch(toggleSideMenu());
   };
 
   const onAddMemberToBoard = event => {
-    toggleModal({ event, type: 'profile' });
+    toggleModal({event, type: 'profile'});
   };
 
   const getAvatarBackground = member => {
-    return { background: `url(${member.imgUrl}) center center / cover` };
+    return {background: `url(${member.imgUrl}) center center / cover`};
   };
 
   return (
@@ -48,8 +47,8 @@ export function BoardHeader({ board }) {
               </button>
             </div>
             <div className="nav-members flex">
-              {members.map(member => (
-                <div style={getAvatarBackground(member)} className={`member-avatar`} key={member._id}></div>
+              {members.map((member, index) => (
+                <div style={getAvatarBackground(member)} className={`member-avatar`} key={index}></div>
               ))}
             </div>
             <div
