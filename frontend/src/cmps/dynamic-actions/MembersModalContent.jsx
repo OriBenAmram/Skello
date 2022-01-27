4dc65561aaf12b740a10938e5a55af55bcfda552
Ori Ben Amram <benamram.ori@gmail.com>
2022-01-27 03:26:16 +0200

    the old dynamic action modal with yuval code

--- frontend/src/cmps/dynamic-actions/MembersModalContent.jsx
+++ frontend/src/cmps/dynamic-actions/MembersModalContent.jsx
@@ -1,51 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams, useHistory } from 'react-router-dom'
-
-import { GrClose } from 'react-icons/gr';
-
+import React, {useState, useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+// Icons & SVG
+import femaleGuest from '../../assets/imgs/female-guest.svg';
+import {GrClose} from 'react-icons/gr';
 // Actions
-import { updateTask } from '../../store/board/board.action.js';
-import { toggleModal } from '../../store/app/app.action.js';
-
-export function MembersModalContent({ board, group, task }) {
-  
+import {updateTask, onSaveBoard} from '../../store/board/board.action.js';
+export function MembersModalContent({board, group, task, toggleModal}) {
   const dispatch = useDispatch();
   const [searchedMemberText, setSearchedMemberText] = useState(null);
-  
-  const { boardId, groupId, taskId } = useParams()
-  console.log('boardId, groupId, taskId:', boardId, groupId, taskId);
-
   const onMemberClick = member => {
     const isExists = task.members.find(currMember => currMember.fullname === member.fullname);
     if (isExists) {
       const newTaskMembers = task.members.filter(currMember => currMember.fullname !== member.fullname);
-      const taskToUpdate = { ...task, members: newTaskMembers };
+      const taskToUpdate = {...task, members: newTaskMembers};
       const activityTxt = `removed ${member.fullname}`;
       dispatch(updateTask(board._id, group.id, task.id, taskToUpdate, activityTxt));
     } else {
       const newTaskMembers = [...task.members, member];
-      const taskToUpdate = { ...task, members: newTaskMembers };
+      const taskToUpdate = {...task, members: newTaskMembers};
       const activityTxt = `added ${member.fullname}`;
       dispatch(updateTask(board._id, group.id, task.id, taskToUpdate, activityTxt));
     }
   };
-
   const getAvatarInnerText = member => {
     if (member.imgUrl) return '';
     return member.fullname.charAt(0).toUpperCase();
   };
-
   const getAvatarBackground = member => {
-    if (member.imgUrl) return { background: `url(${member.imgUrl}) center center / cover` };
+    if (member.imgUrl) return {background: `url(${member.imgUrl}) center center / cover`};
   };
-
   return (
     <section className="members-modal-content">
       <section className="modal-header">
-        <button className="simple-close-btn" onClick={(event) => {
-          dispatch(toggleModal({ event, type: 'members' }))
-        }}>
+        <button className="simple-close-btn" onClick={toggleModal}>
           <GrClose className="btn-content" />
         </button>
         Members
--- frontend/src/cmps/task-details/TaskSideBar.jsx
+++ frontend/src/cmps/task-details/TaskSideBar.jsx
@@ -1,49 +1,54 @@
 import { useState } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-
 import { AiOutlineTags, AiOutlineCheckSquare, AiOutlineFieldTime, AiOutlineCopy } from "react-icons/ai";
 import { IoPersonOutline } from "react-icons/io5";
 import { BsPersonPlus, BsArrowRight, BsArchive, BsSquareHalf } from "react-icons/bs";
 import { MdOutlineAttachment } from "react-icons/md";
 import { BiMicrophone } from "react-icons/bi";
 
-// Actions
-import { toggleModal } from "../../store/app/app.action";
-
+import { DynamicActionModal } from '../dynamic-actions/DynamicActionModal.jsx'
 
 export function TaskSideBar({ task, group, board }) {
-    const dispatch = useDispatch();
-
+    console.log('task:', task);
+    
+    const [modal, setModal] = useState({ isModalOpen: false, type: null });
+    const toggleModal = ({ event, type }) => {
+        // In case the modal is open somewhere
+        if (modal.isModalOpen) {
+            setModal({ ...modal, isModalOpen: false })
+            return
+        }
+        setModal({ isModalOpen: true, type, event })
+    }
     return (
         <section className='side-bar'>
             <section className='add-to-card'>
                 <h3>suggested</h3>
                 <button className="button-link"> <IoPersonOutline /> Join</button>
             </section>
-
             <section className='add-to-card'>
                 <h3>Add to card</h3>
                 <button className="button-link" onClick={(event) => {
-                   dispatch( toggleModal({ event, type: 'members' }))
+                    toggleModal({ event, type: 'members' })
                 }} > <BsPersonPlus />Members</button>
                 <button className="button-link" onClick={(event) => {
-                    dispatch(toggleModal({ event, type: 'labels' }))
+                    toggleModal({ event, type: 'labels' })
                 }} > <AiOutlineTags />Labels</button>
                 <button className="button-link" onClick={(event) => {
-                   dispatch( toggleModal({ event, type: 'checklist' }))
+                    toggleModal({ event, type: 'checklist' })
                 }} > <AiOutlineCheckSquare /> Checklist</button>
                 <button className="button-link" onClick={(event) => {
-                   dispatch( toggleModal({ event, type: 'dates' }))
+                    toggleModal({ event, type: 'dates' })
                 }} > <AiOutlineFieldTime />Dates</button>
                 <button className="button-link" onClick={(event) => {
-                   dispatch( toggleModal({ event, type: 'attachment' }))
+                    toggleModal({ event, type: 'attachment' })
                 }} > <MdOutlineAttachment />Attachment</button>
                 {/* <button className="button-link" onClick={(event) => {
-                    dispatch(toggleModal({ event, type: 'stt' }))
+                    toggleModal({ event, type: 'stt' })
                 }} > <BiMicrophone />Speech To Text</button> */}
                 {(!task.style.backgroundColor && !task.style.backgroundImage?.url) && <button className="button-link" onClick={(event) => {
-                    dispatch(toggleModal({ event, type: 'cover' }))
+                    toggleModal({ event, type: 'cover' })
                 }} > <BsSquareHalf style={{ transform: `rotate(270deg)`, height: '10px' }} />Cover</button>}
+                {modal.isModalOpen && <DynamicActionModal isDetails={true} task={task} group={group} board={board} toggleModal={toggleModal} type={modal.type}event={modal.event} />}
             </section>
             <section className='actions'>
                 <h3>Actions</h3>
