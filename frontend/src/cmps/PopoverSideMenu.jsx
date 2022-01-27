f5709338213033265b5a1af3252da60da2d0d725
Ori Ben Amram <benamram.ori@gmail.com>
2022-01-27 18:54:34 +0200

    basic structure of side menu

--- frontend/src/cmps/PopoverSideMenu.jsx
+++ frontend/src/cmps/PopoverSideMenu.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { HiOutlineSearch } from 'react-icons/hi';
+import { MdOutlinePhotoCameraBack } from 'react-icons/md';
 
 export function PopoverSideMenu({ isSideBarOpen, toggleSideMenu }) {
+  const [isSearchOpen, setSearchState] = useState(false)
+  const [searchText, setSearchText] = useState(null)
+
   return (
     <section className="popover-side-menu" style={isSideBarOpen ? { right: '0px' } : { right: '-400px' }}>
       {/* Header */}
@@ -16,9 +21,21 @@ export function PopoverSideMenu({ isSideBarOpen, toggleSideMenu }) {
         <hr className='bottom-hr' />
       </div>
       <section className='sidemenu-main-content'>
-      {/* Upper Options */}
+        {/* Upper Options */}
         <section className='upper-sidemenu-options'>
-          <button className='primary-link-btn'>Filter cards</button>
+          <button className='primary-link-btn' onClick={() => {
+            setSearchState(!isSearchOpen)
+          }}>
+            <HiOutlineSearch className='primary-icon' />
+            Filter cards
+          </button>
+          <input autoFocus type="text" className={`primary-input filter-cards-input ${(isSearchOpen) ? 'open' : 'closed'}`} onChange={(ev) => {
+            setSearchText(ev.target.value)
+          }} />
+          <button className='primary-link-btn'>
+            <MdOutlinePhotoCameraBack className='primary-icon' />
+            Change background
+          </button>
         </section>
         <section className='background-teaser-section'>
 
