59e43de86cb78f7ecb7e41eb9cf6fd64f10ed138
Yuval <yuvalshai95@gmail.com>
2022-01-29 20:02:47 +0200

    home page responsive

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,17 +7,16 @@ export function HomePage() {
     <section className="homepage">
       <div className="hero flex">
         <div className="hero-content flex column">
-          <h1>Skello helps teams move work forward.</h1>
-          <p>
-            Collaborate, manage projects, and reach new productivity peaks. From high rises to the home
-            office, the way your team works is unique—accomplish it all with Trello.
-          </p>
-          <Link to="/workspace">
-
-            <button>
+          <div className="hero-content-wrapper">
+            <h1>Skello helps teams move work forward.</h1>
+            <p>
+              Collaborate, manage projects, and reach new productivity peaks. From high rises to the home
+              office, the way your team works is unique—accomplish it all with Trello.
+            </p>
+            <Link to="/workspace">
               Start Demo
-          </button>
-          </Link>
+            </Link>
+          </div>
         </div>
         <div>
           <img className="hero-img" src={heroImg} alt="" />
