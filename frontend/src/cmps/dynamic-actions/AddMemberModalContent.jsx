9a29a2da0c4e2575ddbab084a9d84bc4296795da
Yuval <yuvalshai95@gmail.com>
2022-02-22 12:51:26 +0200

    *invite to board QR code

--- frontend/src/cmps/dynamic-actions/AddMemberModalContent.jsx
+++ frontend/src/cmps/dynamic-actions/AddMemberModalContent.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { GrClose } from 'react-icons/gr';
+import QRCode from "react-qr-code";
 
 // Actions
 import { toggleModal } from '../../store/app/app.action';
@@ -10,6 +9,7 @@ import { onSaveBoard } from '../../store/board/board.action';
 import { loadUsers } from '../../store/user/user.actions';
 
 // ICONS
+import { GrClose } from 'react-icons/gr';
 
 export function AddMemberModalContent({ onToggleModal, extraMembers, isExtra }) {
   const dispatch = useDispatch();
@@ -17,6 +17,7 @@ export function AddMemberModalContent({ onToggleModal, extraMembers, isExtra }) 
   const members = useSelector(state => state.userModule.users);
   const board = useSelector(state => state.boardModule.board);
   const [filterBy, setFilterBy] = useState(null);
+  const [isQRShown, setIsQRShown] = useState(false);
 
   useEffect(() => {
     onLoadUsers();
@@ -34,6 +35,7 @@ export function AddMemberModalContent({ onToggleModal, extraMembers, isExtra }) 
 
   const onAddMemberToBoard = (event, member) => {
     if (isExtra) return;
+
     const memberIdx = members.findIndex(memberToFind => {
       return memberToFind._id === member._id;
     });
@@ -82,36 +84,51 @@ export function AddMemberModalContent({ onToggleModal, extraMembers, isExtra }) 
       <section className="modal-header">
         <button
           className="simple-close-btn"
-          onClick={event => {
-            onToggleModal({ event, type: 'addMemberToBoard' });
-          }}>
+          onClick={event => { onToggleModal({ event, type: 'addMemberToBoard' }); }}
+        >
           <GrClose className="btn-content" />
         </button>
-        {(isExtra) ? 'More members' : 'Invite to board'}
+
+        <span>{(isExtra) ? 'More members' : 'Invite to board'}</span>
       </section>
+
       <section className="modal-content">
         <div className="modal-title">
           {!isExtra && <input
             placeholder={`Search users`}
             type="text"
             className="modal-main-input"
-            onChange={ev => {
-              setFilterBy(ev.target.value);
-            }}
+            onChange={ev => { setFilterBy(ev.target.value); }}
             autoFocus
-          />}
-          <h4>Board Members</h4>
-          <section className="users-list">
+          />
+          }
+
+          <div className="flex justify-space-between">
+            <h4 onClick={() => { setIsQRShown(false) }}>Board Members</h4>
+            {!isExtra && <h4
+              className="invite"
+              onClick={() => { setIsQRShown(prevIsQRShown => !prevIsQRShown) }}
+            >
+              invite to board
+            </h4>
+            }
+          </div>
+
+          {!isQRShown && <section className="users-list">
             {getMembersForDisplay().map((member, index) => {
               return (
                 <div
                   key={index}
                   className="user-preview"
                   onClick={event => {
-                    (isExtra) ? dispatch(toggleModal({ event, type: 'otherMemberModal', member, isShown: true, posYAddition: -150, posXAddition: -200 })) : onAddMemberToBoard(event, member)
-
-
+                    (isExtra) ?
+                      dispatch(toggleModal({
+                        event, type: 'otherMemberModal', member, isShown: true,
+                        posYAddition: -150, posXAddition: -200
+                      })) :
+                      onAddMemberToBoard(event, member)
                   }}>
+
                   <div
                     className={`member-avatar ${member.imgUrl ? 'with-image' : ''}`}
                     style={getAvatarBackground(member)}></div>
@@ -119,7 +136,11 @@ export function AddMemberModalContent({ onToggleModal, extraMembers, isExtra }) 
                 </div>
               );
             })}
-          </section>
+          </section>}
+
+          {isQRShown && <section className="qr-code">
+            <QRCode value={`https://skello.herokuapp.com/board/${board._id}`} />
+          </section>}
         </div>
       </section>
     </section>
